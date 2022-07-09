692ea3b443c8f75ee3cfd03c67dbb2d739768576
javirecamp <javirecamp@gmail.com>
2022-07-10 07:54:52 +0900

    study source

---
+++ lab04-JSX/jsx01.js
@@ -0,0 +1,158 @@
+// JSX code
+const element = <h1>Hello World~!!</h1>;
+
+// JSX code -> Javascript code로 변환하는 함수
+// React.createElement()
+React.creatElement(
+    type,     // div, h1 등 html tag의 종류
+    [props],  // html tag의 속성
+    [...children] // 현재 element가 포함하고 있는 자식 element
+)
+
+// 간단한 JSX 적용/미적용 예제
+// JSX 적용 code
+const element = (
+    <h1 className="greeting">
+        Hello, World~!!
+    </h1>
+);
+
+// JSX 미적용 code
+const element = React.createElement(
+    'h1',
+    { className : 'greeting' },
+    'Hello, World~!!'
+);
+
+// React.createElement()의 결과로 생성되는 객체
+const element = {
+    type: 'h1',
+    props: {
+        className: 'greeting',
+        children: 'Hello, world~!!'
+    }
+}
+
+
+// React Hello 컴포넌트 생성 - JSX code 
+class Hello extends React.Component{
+    render(){
+        return <div>Hello {this.props.toWhat}</div>;
+    }
+}
+
+ReactDOM.render(
+    <Hello toWhat="World" />,
+    document.getElementById("root")
+);
+
+// React Hello 컴포넌트 생성 - JSX 미적용 code 
+class Hello extends React.Component{
+    render(){
+        return React.creatElement('div',null,`Hello ${this.props.toWhat}`);
+    }
+}
+
+ReactDOM.render(
+    React.creatElement(Hello, {toWhat: 'World'}, null),
+    document.getElementById('root')
+);
+
+// JSX 사용함 
+<div>Hello, {name}</div>
+
+// JSX 사용안함
+React.createElement('div',null, `Hello, ${name}`);
+
+
+// Injection Attacks 해킹을 방어할 수 있다.
+const title = response.potentiallyMaliciousInput;
+
+// 이 코드는 안전합니다. 
+const element = <h1>{title}</h1>;
+
+// JSX Code : JS + XML/HTML 
+const name = "아이유";
+const element = <h1>안녕, {name}</h1>;
+
+ReactDOM.render(
+    element,
+    document.getElementById('root');
+);
+
+// JSX code 
+function formatName(user){
+    return user.firstName + '' + user.lastName;
+};
+
+const user = {
+    firstName: "Jieun",
+    lastName: "Lee"
+};
+
+const element = (
+    <h1>
+        Hello, {formatName(user)}
+    </h1>
+);
+
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+);
+
+// JSX code
+function getGreeting(user){
+    if(user){
+        return <h1>Hello, {formatName(user)}</h1>
+    }
+    return <h1>Hello, Stranger.</h1>
+}
+
+// element의 속성에 값을 넣는 방법 
+// 더블 쿼테이션 사이에 문자열을 넣는다.
+const element = <div tabIndex="0"></div>;
+
+// 중괄호({}) 사이에 JavaScript Code를 작성 
+const element = <img src={user.avatarUrl}></img>;
+
+
+// 자식(children)을 정의하는 방법
+const element = (
+    <div>
+        <h1>브로커</h1>
+        <h2>이지은, 송강호가 나온 영화</h2>
+    </div>
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
